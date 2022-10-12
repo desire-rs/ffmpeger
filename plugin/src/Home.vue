e398a66a41273fec1a83c927d598d3fd8e19884e
Mankong zhao <mankong.zhao@aax.com>
2022-10-12 23:48:01 +0800

    fix plugin

--- plugin/src/Home.vue
+++ plugin/src/Home.vue
@@ -1,45 +1,106 @@
 <template lang="pug">
 .container
-  textarea(v-model="value")
-  button(@click="submit") 确定
+  .control
+    button.btn(@click="submit") submit
+    button.btn(@click="clear") clear
+  .content
+    .item(v-for="(url, index) in urls")
+      input(type="checkbox"  v-model="targets" :value="url")
+      label {{url}}
+    textarea(cols='30' rows='5' v-model="info")
+    textarea(cols='30' rows='4' style='color:red;' v-model="result")
 </template>
 
 <script lang="ts">
-
 export default {
   data() {
     return {
-      name: 'Home',
-      value: "{}",
-      m3u8s: [],
+      title: "title", // title
+      urls: [], // 捕获的urls
+      targets: [], // 选中的urls
+      info: "{}", // 日志信息
+      result: "{}", // 结果信息
     };
   },
+  mounted() {
+    this.init();
+  },
   methods: {
+    async init() {
+      const { m3u8 } = await chrome.storage.local.get("m3u8");
+      console.log(m3u8);
+      if (!!m3u8) {
+        this.urls = JSON.parse(m3u8);
+      }
+    },
+    async clear() {
+      const result = confirm("确定清空内容?");
+      if (result) {
+        await chrome.storage.local.clear();
+      }
+    },
     async sendToContent(params) {
       let [tab] = await chrome.tabs.query({
         active: true,
-        currentWindow: true
+        currentWindow: true,
       });
       params.url = tab.url;
       params.title = tab.title;
       if (tab && tab.id) {
         const port = chrome.tabs.connect(tab.id, {
-          name: 'ffmpeger'
+          name: "ffmpeger",
         });
         port.postMessage(params);
         port.onMessage.addListener((msg) => {
-          console.log('response:', msg);
-          this.value = JSON.stringify(msg, null, 2);
+          console.log("response:", msg);
+          this.info = JSON.stringify(msg, null, 2);
         });
       }
     },
     async submit() {
-      await this.sendToContent({ msg: "Hello", source: "popup" })
-    }
+      for (const m3u8 of this.targets) {
+        const data = { url: m3u8, title: this.title };
+        await this.sendToContent(data);
+      }
+    },
   },
 };
 </script>
 
 <style lang="styl" scoped>
-
+html,
+body
+  margin: 0;
+  padding: 0;
+.container
+  display: flex;
+  flex-direction: column;
+  flex-wrap: wrap;
+.content
+  display: flex;
+  flex-direction: column;
+  flex-wrap: wrap;
+body
+  margin: 0;
+  padding: 10px;
+textarea
+  width: 700px;
+  margin-top: 5px;
+  border-radius: 4px;
+  padding: 5px;
+  outline: none;
+label
+  color: #000;
+  background-color: #ccc;
+  margin: 2px 0;
+  width: 710px;
+button
+  cursor: pointer;
+  padding: 8px 18px;
+  border: none;
+  border-radius: 4px;
+  outline: none;
+  color: white;
+  margin: 0 5px;;
+  background-color: cornflowerblue;
 </style>
--- plugin/src/scripts/background.ts
+++ plugin/src/scripts/background.ts
@@ -1,10 +1,22 @@
-const DEFAULT_API = 'http://0.0.0.0:12306';
+interface IMessage {
+  api: string;
+  method: string;
+  data?: string;
+}
+const DEFAULT_BASE_URL = 'http://0.0.0.0:12306';
 chrome.webRequest.onResponseStarted.addListener(async (details) => {
   const { method, url, initiator, tabId, requestId, frameId } = details;
   if (method === 'GET' && isMatch(url, '.m3u8')) {
     console.log(url, initiator);
+    let { m3u8 } = await chrome.storage.local.get('m3u8');
+    if (!!m3u8) {
+      m3u8 = JSON.parse(m3u8);
+    } else {
+      m3u8 = [];
+    }
+    m3u8.push(url);
     await chrome.storage.local.set({
-      m3u8: url
+      m3u8: JSON.stringify(Array.from(new Set(m3u8)))
     });
   }
   if (method === 'GET' && isMatch(url, '.mp4')) {
@@ -20,17 +32,31 @@ function isMatch(url, target) {
   return url.indexOf(target) > -1;
 }
 
-chrome.runtime.onMessage.addListener(async (message, sender, sendResponse) => {
-  let { api } = await chrome.storage.local.get('api');
-  const result = await fetch(api ?? DEFAULT_API, {
-    method: 'POST',
-    body: JSON.stringify(message),
-    headers: {
-      'Content-Type': 'application/json; charset=utf-8'
-    },
-  });
+chrome.runtime.onMessage.addListener(async (message: IMessage, sender, sendResponse) => {
+  let { baseUrl } = await chrome.storage.local.get('baseUrl');
+  let { method, api, data } = message;
+  let result;
+  const url = `${baseUrl ? baseUrl : DEFAULT_BASE_URL}/${api}`;
+  if (method.toUpperCase() === 'GET') {
+    result = await fetch(url, {
+      method: method,
+      headers: {
+        'Content-Type': 'application/json; charset=utf-8'
+      },
+    });
+  } else if (method.toUpperCase() === 'POST') {
+    result = await fetch(url, {
+      method: method,
+      body: JSON.stringify(data),
+      headers: {
+        'Content-Type': 'application/json; charset=utf-8'
+      },
+    });
+  }
+  const body = await result.json();
+  console.log('fetch api result', body);
   setTimeout(async () => {
-    sendResponse(result);
+    sendResponse(body);
   }, 200);
   return true;
 });
