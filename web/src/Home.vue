5765dbbdf851ad4d00bf83e35f5c29f020ca4c4f
Mankong.Zhao <mankong.zhao@aax.com>
2022-10-14 16:20:23 +0800

    fix web

--- web/src/Home.vue
+++ web/src/Home.vue
@@ -1,6 +1,20 @@
 <template lang="pug">
 .container
-  h1 Home
+  table
+    thead
+      tr
+        th id
+        th title
+        th url
+        th status
+        th createdAt
+    tbody
+      tr(v-for="task in tasks")
+        td(v-text="task.id")
+        td(v-text="task.title")
+        td(v-text="task.url")
+        td(v-text="task.status")
+        td(v-text="task.createdAt")
 
 </template>
 
@@ -25,4 +39,31 @@ export default {
 };
 </script>
 
-<style lang="styl" scoped></style>
+<style lang="styl" scoped>
+.container
+  margin: 10px 0;
+.container table
+  width: 100%;
+  border-collapse: collapse;
+.container caption
+  height: 30px;
+  line-height: 30px;
+  font-weight: 700;
+.container thead th,
+.container tbody td
+  padding:8px;
+  height: 19px;
+  line-height: 19px;
+  font-weight: 400;
+.container thead th
+  text-align: left;
+  color:#fff;
+  background-color: #353535;
+.container tbody tr
+  background-color: #fefefe;
+  color: #686868;
+.container tbody tr:nth-child(even)
+  background-color: #f2f2f2;
+.row-selected
+  width: 15px;
+</style>
